e1367f14162350afd33995b21b55ec94c994dd4f
srihari <udugani@gmail.com>
2022-06-06 19:58:12 +0530

    Fixes #5 and #6

---
+++ creator/dag_creator.py
@@ -0,0 +1,38 @@
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import os
+
+from creator.validate_variables import VariableValidator
+
+
+class DagCreator:
+
+    def __init__(self, template_file, config_file, dag_home):
+        self.template_file_path = template_file
+        self.config_file_path = config_file
+        self.dag_home = dag_home
+
+    def create(self):
+        # check the variables in template file and YAML config keys for a match
+        validator = VariableValidator(self.template_file_path, self.config_file_path)
+        if not validator.validate():
+            print("Template variables are not matching with YAML configuration")
+            print(f"DAG file generation for {self.template_file_path} and {self.config_file_path} failed")
+            return
+
+        template_dir = os.path.dirname(os.path.abspath(self.template_file_path))
+        template_file_name = os.path.basename(os.path.abspath(self.template_file_path))
+
+        env = Environment(loader=FileSystemLoader(template_dir))
+        ref_template = env.get_template(template_file_name)
+
+        with open(self.config_file_path, 'r') as config_file_ref:
+            yaml_config = yaml.safe_load(config_file_ref)
+            dag_file_path = os.path.join(self.dag_home, f"dag_{yaml_config['dag_id']}.py".lower())
+
+            print(f'Generating DAG file for {self.template_file_path} and {self.config_file_path} at {self.dag_home}')
+
+            with open(dag_file_path, 'w') as dag_file_ref:
+                dag_file_ref.write(ref_template.render(yaml_config))
+                print(f'DAG file {dag_file_path} is generated successfully')
---
+++ dynamic_dag.py
@@ -0,0 +1,75 @@
+
+import argparse
+
+import yaml
+from creator.dag_creator import DagCreator
+
+
+class AbstractDynamicDagCreator:
+
+    def __init__(self, args: dict):
+        self.creator_args = args
+
+    def create(self):
+        pass
+
+
+class SingleDynamicDagCreator(AbstractDynamicDagCreator):
+    """ Dynamic DAG creation based one combination of template and yaml config file """
+    def create(self):
+        creator = DagCreator(self.creator_args['template_file'],
+                    self.creator_args['config_file'],
+                    self.creator_args['dag_home'])
+        
+        creator.create()
+
+
+class MultipleDynamicDagCreator(AbstractDynamicDagCreator):
+    """ Dynamic DAG creation based on a profile based mapping configuration file """
+    def create(self):
+        with open(self.creator_args['profile_file'], 'r') as profilefile:
+            content = yaml.safe_load(profilefile)
+        
+            for key in content.keys():
+                template_file = content[key]['template_file']
+                config_file = content[key]['config_file']
+                print(template_file, config_file)
+                creator = DagCreator(template_file, config_file, self.creator_args['dag_home'])
+                creator.create()
+
+
+class DynamicDagFactory():
+
+    @staticmethod
+    def get_dag_creator(args: dict) -> AbstractDynamicDagCreator:
+        if args['cmd'] == 'single':
+            return SingleDynamicDagCreator(args)
+        elif args['cmd'] == 'multiple':
+            return MultipleDynamicDagCreator(args)
+        else:
+            raise ValueError('Unknown command')
+
+
+# Command Line arguments
+# Flag - Single or Multiple should be mutually exclusive
+# For Single, template_file and yaml_file path should be provided
+# For multiple, profile_file path should be provided
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dynamic DAG Creation')
+    parser.add_argument('-d', '--dag_home', required=True, help='Absolute path to folder where DAG are created')
+
+    sp = parser.add_subparsers(title='sub commands', description='valid commands', help='additional help')
+    single_dd_parser = sp.add_parser('single')
+    multiple_dd_parser = sp.add_parser('multiple')
+
+    single_dd_parser.set_defaults(cmd='single')
+    multiple_dd_parser.set_defaults(cmd='multiple')
+
+    single_dd_parser.add_argument('-t', '--template_file', required=True, help='Absolute path to the template file')
+    single_dd_parser.add_argument('-c', '--config_file', required=True, help='Absolute path to the YAML configuration file')
+
+    multiple_dd_parser.add_argument('-p', '--profile_file', required=True, help='Absolute path to the profile file')
+
+    cmd_args = parser.parse_args()
+    creator = DynamicDagFactory.get_dag_creator(vars(cmd_args))
+    creator.create()
